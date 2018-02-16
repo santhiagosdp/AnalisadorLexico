ae332b711bd4be700012db5791f2a91c52aa505f
santhiagosdp <santhiagosdp@gmail.com>
2018-02-16 19:01:56 -0200

    começou a verificar
    verificando qual tpo e apresentando

--- js/app.js
+++ js/app.js
@@ -8,7 +8,10 @@ CEULP ULBRA
 
 
 function verif() 
-{	var keyReservada = ["and","del"];	
+{	var keyReservada = ["and","del","from","not","while","as","elif","global","or","with","assert","else","if","object","pass","yield","break","except",
+"import","print","class","exec","in","len","range","raise","continue","finally","is","list","return","def","for","lambda","try","self","split"];
+	var keyIdentificador = ["+","-","*","**","/","//","%","<<",">>","&","|","^","~","<",">","<=",">=","==","!=","<>"];
+	var keyDelimitador = ["(",")","[","]","{","}","@",",",":",".","`","=",";","+=","-=","*=","/=","//=","%=","&=","|=","^=",">>=","<<=","**=","'","'"];
 	
 	codigo = document.getElementById('codigo').value;
 	numElem = 0;
@@ -18,11 +21,16 @@ function verif()
 	
 	for(var i=0; i < keys.length; i++)
 			for (var j=0; j < keyReservada.length; j++)
+				
 				if(keys[i] == keyReservada[j]){
-						relatorio += keys[i] + ' - palavra reservada';
+					relatorio += keys[i] + ' - palavra reservada \n\n\n';
 				}
-		
-	
+				else if (keys[i] == keyIdentificador[j]){
+					relatorio += keys[i] + ' - Identifiacor\n';
+				}
+				else if (keys[i] == keyDelimitador[j]){
+					relatorio += keys[i] + ' - Delimitador\n\n';
+				}					
 	if (2==2)
 	{
 		document.getElementById('result').innerHTML = relatorio;
