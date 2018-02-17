74b566ea7629d047aa50a92a586da18d82402501
santhiagosdp <santhiagosdp@gmail.com>
2018-02-17 16:55:05 -0200

    melhora tabela
    tabelsa refresh
    
    Co-Authored-By: Felipe Cabral <felipecabrals@users.noreply.github.com>
    Co-Authored-By: macielsousa <macielsousa@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,49 +1,53 @@
- /*  
-COMPILADORES 2018/1
-Sistemas de Informação
-CEULP ULBRA
-*/
+<!--
+//COMPILADORES 2018/1
+//Sistemas de Informação
+//CEULP ULBRA
+-->
 
-//analisando 
+//analisando
 
-function verif() 
-{ // Alfabeto Python	
+function verif()
+{ // Alfabeto Python
 	var keyReservada = ["and", "del","from","not","while","as","elif","global","or","with","assert","else","if","object","pass","yield","break","except",
 	"import","print","class","exec","in","len","range","raise","continue","finally","is","list","return","def","for","lambda","try","self","split"];
 	var keyIdentificador = ["+","var", "i","-","*","**","/","//","%","<<",">>","&","|","^","~","<",">","<=",">=","==","!=","<>"];
 	var keyDelimitador = ["(",")","[","]","{","}","@",",",":",".","`","=",";","+=","-=","*=","/=","//=","%=","&=","|=","^=",">>=","<<=","**=","'","'"];
-	
+
 	codigo = document.getElementById('codigo').value;
 	numElem = 0;
-	relatorio="";
+	relatorio="<table class='table table-bordered'><tr><th>Token</th><th>Identificação</th></tr>";
 	numElemStr="";
 	keys = codigo.split(' ');
-	
+
 	for(var i=0; i < keys.length; i++){
 		var entrou = false;
 		for (var r=0; r < keyReservada.length; r++){
 			if(keys[i] == keyReservada[r] && entrou == false){
-				relatorio += keys[i] + ' ==>  palavra reservada <br></br> ';
+				relatorio += "<tr><td>"+keys[i]+"</td><td>Palavra reservada"+"</td></tr> "
+				//relatorio += keys[i] + ' ==>  palavra reservada <br></br> ';
 				entrou = true;
 				break;
 			}
 		}
 		for (var j=0; j < keyIdentificador.length; j++){
 			if (keys[i] == keyIdentificador[j] && entrou == false){
-				relatorio += keys[i] + ' ==>  Identificador<br></br>';
+				relatorio += "<tr><td>"+keys[i]+"</td><td>Identificador"+"</td></tr> "
+				//relatorio += keys[i] + ' ==>  Identificador<br></br>';
 				entrou = true;
 				break;
 			}
 		}
 		for (var d=0; d < keyDelimitador.length; d++){
 			if (keys[i] == keyDelimitador[d] && entrou == false){
-				relatorio += keys[i] + ' ==> Delimitador<br></br>';
+				relatorio += "<tr><td>"+keys[i]+"</td><td>Delimitador"+"</td></tr> "
+				//relatorio += keys[i] + ' ==> Delimitador<br></br>';
 				entrou = true;
 				break;
 			}
 		}
 		if (entrou ==false){
-			relatorio += keys[i] + ' ==> Sem êxito na analise<br></br>';
+			relatorio += "<tr><td>"+keys[i]+"</td><td>Sem êxito na analise"+"</td></tr> "
+			//relatorio += keys[i] + ' ==> Sem êxito na analise<br></br>';
 			entrou = true;
 		}
 	}
@@ -56,4 +60,4 @@ function verif()
 	{
 		document.getElementById('result').innerHTML=relatorio;
 	}
-} 
+}
